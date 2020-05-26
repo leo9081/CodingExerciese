a3f29d29e30c9e4189a1886de29c7ddbb0a53aaa
leo9081 <leo9081@ksu.edu>
2020-05-25 21:02:59 -0700

    Coding Practice

--- app/src/main/java/com/yi/interviewquestions/ByteDance/TrapWater.java
+++ app/src/main/java/com/yi/interviewquestions/ByteDance/TrapWater.java
@@ -1,4 +1,31 @@
 package com.yi.interviewquestions.ByteDance;
 
 public class TrapWater {
+    public int trap(int[] height) {
+        int left = 0; int right = height.length-1;
+        int leftMax = 0; int rightMax = height.length-1;
+        int result = 0;
+
+        while(left < right){
+            if(height[left] < height[right]){
+                if (height[leftMax] <= height[left]) {
+                    leftMax = left;
+                } else {
+                    result += height[leftMax] - height[left];
+                }
+                left++;
+            }else{
+                if (height[rightMax] <= height[right]) {
+                    rightMax = right;
+                } else {
+                    result += height[rightMax] - height[right];
+                }
+                right--;
+            }
+
+        }
+
+        return result;
+    }
+
 }
